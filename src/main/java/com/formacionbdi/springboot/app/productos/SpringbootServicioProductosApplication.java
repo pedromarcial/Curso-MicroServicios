04140b9b76f10dc21aca45be2916d452951a2a80
Peter217 <pedro.marcial.p@outlook.com>
2020-10-22 12:55:32 -0500

    cleaning text

--- src/main/java/com/formacionbdi/springboot/app/productos/SpringbootServicioProductosApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/productos/SpringbootServicioProductosApplication.java
@@ -12,5 +12,4 @@ public class SpringbootServicioProductosApplication {
 		SpringApplication.run(SpringbootServicioProductosApplication.class, args);
 	}
 
-	// Se agregan pruebas otro test blaasjlash
 }
