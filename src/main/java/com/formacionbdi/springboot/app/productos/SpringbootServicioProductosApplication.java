90df7af5f267dbbdf8201d707c920c14522b8357
Peter217 <pedro.marcial.p@outlook.com>
2020-10-22 12:38:03 -0500

    test nueva sincronizacion

--- src/main/java/com/formacionbdi/springboot/app/productos/SpringbootServicioProductosApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/productos/SpringbootServicioProductosApplication.java
@@ -1,6 +1,8 @@
 package com.formacionbdi.springboot.app.productos;
 
 import org.springframework.boot.SpringApplication;
+
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
